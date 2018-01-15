c0f6ef97e413225ef8384980c72a23aeb95ba16c
Adam Rutkowski <a_rutkowski@outlook.com>
2018-01-15 22:20:40 +0100

    Add typings

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+// Type definitions for webpack-md5-hash
+// Project: webpack-md5-hash
+
+import * as webpack from 'webpack';
+
+declare namespace WebpackMd5Hash { }
+
+declare class WebpackMd5Hash extends webpack.Plugin {
+  constructor();
+}
+
+export = WebpackMd5Hash;
